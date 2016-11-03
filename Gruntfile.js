38e76929bf2e1f3c3d06f63b0e90512cbde09345
Kevin Thornbloom <kevin@kthornbloom.com>
2016-11-03 14:01:04 -0400

    starting some things...
    … ’n stuff

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		/* COMBINE JS FILES
+		=========================================*/
+		concat: {
+			dist: {
+				src: [
+					'js/src/*.js'
+				],
+				dest: 'js/build/production.js'
+			}
+		},
+
+		/* MINIFY JS FILES
+		=========================================*/
+		uglify: {
+			build: {
+				src: 'js/build/production.js',
+				dest: 'js/build/production.min.js'
+			}
+		},
+
+		/* CONVERT SASS TO CSS
+		=========================================*/
+		sass: {
+			dist: {
+				files: {
+					'css/build/stylesheet.css': 'css/src/~global.scss'
+				}
+			}
+		},
+
+		/* ADD CSS VENDOR PREFIXES
+		=========================================*/
+		autoprefixer: {
+            dist: {
+                src: 'css/build/stylesheet.css'
+            }
+        },
+
+		/* MINIFY CSS
+		=========================================*/
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'css/build',
+					src: ['*.css', '!*.min.css'],
+					dest: 'css/build',
+					ext: '.min.css'
+				}]
+			}
+		},
+
+		/* WATCH FILES FOR CHANGES, THEN RUN TASKS
+		=========================================*/
+		watch: {
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false
+				},
+			},
+			css: {
+				files: ['css/src/*.scss'],
+				tasks: ['sass','autoprefixer','cssmin'],
+				options: {
+				  spawn: false
+				}
+			},
+			js: {
+				files: ['js/src/*.js'],
+				tasks: ['concat','uglify'],
+				options: {
+				  spawn: false
+				}
+			},
+			html:{
+				files: ['./**/*.html'],
+				tasks: [],
+				options: {
+				  spawn: false,
+				  livereload: true
+				}
+			}
+		},
+
+		/* SYNCRONIZE BROWSERS & LIVE INJECT CSS / UPDATE FOR JS & HTML
+		=========================================*/
+		browserSync: {
+		    files: ["css/build/*.css", "js/build/*.js", "*.html"],
+		    options: {
+				watchTask: true,
+		        server: './'
+			}
+		}
+
+	});
+
+	/* TELL GRUNT WHICH PLUGINS WE NEED
+	=========================================*/
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+	grunt.registerTask('default', ['concat','uglify','browserSync','watch','sass','autoprefixer','cssmin']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # Cottontail-2
 For use with JR CMS 4+
+
+<table>
+	<tr>
+		<td>row</td>
+		<td>Make a row</td>
+	</tr>
+	<tr>
+		<td>row-{value}</td>
+		<td>Value denotes when the row's cells should become full width. Defaults are 512, 720, 960</td>
+	</tr>
+	<tr>
+		<td>row-stepped-{value}</td>
+		<td>Value denotes when the row's cells should become full width. Cells will be 50% width at the next highest breakpoint. Defaults are 512, 720, 960, 1024</td>
+	</tr>
+	
+	<tr>
+		<td>col-{value}</td>
+		<td>Value denotes column width. Defaults are 1 through 12.</td>
+	</tr>
+	<tr>
+		<td>spaced</td>
+		<td>Add this class to the <b>row</b> to add margins around the <b>columns</b></td>
+	</tr>
+	<tr>
+		<td>align-{value}</td>
+		<td>Add to a column to align the content. Defaults are br, bl, tr, cr, cl, c. These stand for bottom right, centered right, etc.</td>
+	</tr>
+	<tr>
+		<td>align-{value 1}-{value 2}</td>
+		<td>Add to a column to align the content at a certain width. Defaults for value 1 are br, bl, tr, cr, cl, c. These stand for bottom right, centered right, etc.<br>
+		Defaults for value 2 are 512, 720, 960</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
